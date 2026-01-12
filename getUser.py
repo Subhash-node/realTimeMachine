2dca6291af5dfa44891e689976c3d21c2214f24d
Subhash-node <subh.onlyme@gmail.com>
2026-01-12 22:15:57 +0530

    commit code

--- getUser.py
+++ getUser.py
@@ -6,3 +6,7 @@ def getUserSettings():
     return {"language": "en", "privacy": "high"}
 def getUserActivity():
     return {"lastLogin": "2024-06-01", "posts": 42}
+def getUserHistory():
+    return {"previousLogins": 15, "comments": 128}
+def getUserNotifications():
+    return {"unreadMessages": 5, "alerts": 2}
